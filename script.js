b33c304987a008b57a02c169f0d2b52a5f83b7f8
Hmiklo <165051810+Hmiklo@users.noreply.github.com>
2025-04-09 14:12:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.addEventListener('scroll', function () {
     const logoContainer = document.querySelector('.logo-container');
+    const text = document.querySelector('.text');
     const scrollY = window.scrollY;
     const maxScroll = document.body.scrollHeight - window.innerHeight;
-
-    const scrollProgress = scrollY / maxScroll;
-
-    const scaleFactor = 1 + scrollProgress * 150; 
-    const minScale = 1;
-    const maxScale = 600;
-    const scale = Math.min(Math.max(scaleFactor, minScale), maxScale);
-
-    const moveX = 0; // не двигаем по оси X
-    const moveY = scrollProgress * 0; // сдвиг строго вниз
-
-    logoContainer.style.transform = `translate(-50%, calc(-50% + ${moveY}px)) scale(${scale})`;
-});
+  
+    const scrollProgress = scrollY / maxScroll; // 0 до 1
+  
+    // Масштаб логотипа
+    const scaleFactor = 1 + scrollProgress * 300;
+    logoContainer.style.transform = `translate(-50%, -50%) scale(${scaleFactor})`;
+  
+    // Прозрачность текста
+    const fadeInProgress = Math.min(scrollProgress / 0.01, 1); // 5% скролла = полная видимость
+    text.style.opacity = fadeInProgress;
+  });
+  
